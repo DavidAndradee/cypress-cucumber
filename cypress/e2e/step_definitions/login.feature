8151beb4cd0c571f27e22c658806447676353fea
David Andrade <david1999andrade@gmail.com>
2024-08-27 13:57:15 -0300

    add specs

--- cypress/e2e/step_definitions/login.feature
+++ cypress/e2e/step_definitions/login.feature
@@ -2,7 +2,12 @@ Feature: Login
 
 Scenario: Valid Login
 
-Given eu realizo o login na pagina
+Given eu acesso a pagina de login
 When eu informo meu email e password
 Then eu espero sucesso no login
-    Feature Description
+
+Scenario: Invalid Login
+
+Given eu acesso a pagina de login
+When eu informo meu email e uma senha incorreta
+Then eu espero que o login falhe
--- cypress/e2e/step_definitions/login/login.js
+++ cypress/e2e/step_definitions/login/login.js
@@ -1,6 +1,6 @@
-import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
-Given('eu realizo o login na pagina', ()=>{
+Given('eu acesso a pagina de login', ()=>{
     cy.visit("http://localhost:3000/")
 })
 
@@ -11,6 +11,23 @@ When("eu informo meu email e password", ()=>{
 })
 
 Then("eu espero sucesso no login", ()=>{
-    cy.get('h1').contains('Minhas Tarefas')
+    cy.get('h1').contains('Minhas tarefas')
     cy.get('.sc-Axmtr').should('be.visible')
 })
+
+And
+
+Given('eu acesso a pagina de login', ()=>{
+    cy.visit("http://localhost:3000/")
+})
+
+When("eu informo meu email e uma senha incorreta", ()=>{
+    cy.get(':nth-child(3) > [data-testid="input-container"]').type("david1999andrade@gmail.com")
+    cy.get(':nth-child(4) > [data-testid="input-container"]').type("teste3")
+    cy.get('.sc-Axmtr').click()
+})
+
+Then("eu espero que o login falhe", ()=>{
+    cy.get('.notice').contains('Ocorreu um erro ao fazer login, verifique suas credenciais')
+})
+
